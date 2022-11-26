20eefa4999dac2e4385c49e868c6f963760328c7
Apoorve\Edureka <techinnerview@gmail.com>
2022-11-26 16:02:51 +0530

    Backend classes Handson

---
+++ Module 3/Backend/fs.js
@@ -0,0 +1,6 @@
+var fs = require("fs");
+
+fs.writeFileSync("fsTextFile.txt", "This is an example of FS Package");
+fs.writeFileSync("fsTextFile2.txt", "This is an example of FS Package");
+fs.writeFileSync("fsTextFile3.txt", "This is an example of FS Package");
+
---
+++ Module 3/Backend/os.js
@@ -0,0 +1,4 @@
+var os = require("os");
+console.log("Platform of Running OS is: " + os.platform());
+console.log("Architecture of Running OS is: " + os.arch());
+console.log("Running OS is: " + os.uptime());
