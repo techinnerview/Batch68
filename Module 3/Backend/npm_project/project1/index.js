c2d7c6ce2ee5186046ba1f4ac160f02d31d03327
Apoorve\Edureka <techinnerview@gmail.com>
2022-11-27 15:27:51 +0530

    NPM Handson

---
+++ Module 3/Backend/npm_project/project1/index.js
@@ -0,0 +1,26 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue("Hello Everyone"));
+console.log(chalk.blue("WELCOME TO EDUREKA"));
+console.log(chalk.underline.red("WELCOME TO EDUREKA"));
+console.log(chalk.underline.yellow("WELCOME TO EDUREKA"));
+console.log(chalk.green("WELCOME TO EDUREKA"));
+console.log(chalk.blue("WELCOME TO EDUREKA"));
+console.log(chalk.underline.red("WELCOME TO EDUREKA"));
+console.log(chalk.underline.yellow("WELCOME TO EDUREKA"));
+console.log(chalk.green("WELCOME TO EDUREKA"));
+console.log(chalk.blue("WELCOME TO EDUREKA"));
+console.log(chalk.underline.red("WELCOME TO EDUREKA"));
+console.log(chalk.underline.yellow("WELCOME TO EDUREKA"));
+console.log(chalk.green("WELCOME TO EDUREKA"));
+console.log(chalk.blue("WELCOME TO EDUREKA"));
+console.log(chalk.underline.red("WELCOME TO EDUREKA"));
+console.log(chalk.underline.yellow("WELCOME TO EDUREKA"));
+console.log(chalk.green("WELCOME TO EDUREKA"));
+console.log(chalk.blue("WELCOME TO EDUREKA"));
+console.log(chalk.underline.red("WELCOME TO EDUREKA"));
+console.log(chalk.underline.yellow("WELCOME TO EDUREKA"));
+console.log(chalk.green("WELCOME TO EDUREKA"));
+console.log(chalk.blue("WELCOME TO EDUREKA"));
+console.log(chalk.underline.red("WELCOME TO EDUREKA"));
+console.log(chalk.underline.yellow("WELCOME TO EDUREKA"));
